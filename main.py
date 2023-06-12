f6fa7513b5f26a527ede3b587473d4cb8d328c6d
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2023-06-12 21:18:25 +0800

    NewCode

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import tkinter as tk
+
+root = tk.Tk()# basically main window constructor
+
+root.geometry("800x600") # set resolution x,y value
+root.title("FirstGui")
+root.mainloop() # call the constructor
